47dc28a322386295005c7e469aea0ee4d009fceb
thaihanguyen98 <142948159+thaihanguyen98@users.noreply.github.com>
2023-09-12 15:16:53 +1000

    add AM PM feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
+
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -8,6 +9,7 @@ function formatDate(timestamp) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+
   let days = [
     "Sunday",
     "Monday",
@@ -18,7 +20,29 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes} `;
+  let dayNumber = date.getDate();
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let year = date.getFullYear();
+
+  let ampm = hours >= 12 ? `PM` : `AM`;
+  hours = hours ? hours : 12;
+
+  return `${day}, ${dayNumber} ${month} ${year} | ${hours} : ${minutes} ${ampm}`;
 }
 
 function showWeather(response) {
